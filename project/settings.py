f71bc0f21d4e1885b4320e8c537f04ec0591f24c
ragnarranjan <ragnarranjan007@gmail.com>
2021-03-02 11:22:23 +0530

    added products app

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'accounts',
+    'products'
+
 ]
 
 # AUTH_USER_MODEL = 'accounts.User' #changes the built-in user model to ours
